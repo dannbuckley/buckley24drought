a6351d1d1c355852e3564e5403e418a375d487eb
Daniel Buckley <buckley.daniel.n@gmail.com>
2024-01-17 10:32:45 -0700

    Add types to conftest for autocomplete info

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,17 @@
+from typing import List
+
 import pytest
 
 SUITES = ("slow", "intg")
 
-def pytest_addoption(parser):
-    """
-    A pytest hook function that allows us to add additional command line arguments
-    and manage which tests are run.
+
+def pytest_addoption(parser: pytest.Parser):
+    """A pytest hook function that allows us to add additional
+    command line arguments and manage which tests are run.
+
+    Parameters
+    ----------
+    parser : pytest.Parser
     """
     for suite in SUITES:
         parser.addoption(
@@ -22,18 +28,25 @@ def pytest_addoption(parser):
         help="run all tests",
     )
 
-def pytest_configure(config):
-    """
-    Mark tests with the suite name.
+
+def pytest_configure(config: pytest.Config):
+    """Mark tests with the suite name.
+
+    Parameters
+    ----------
+    config : pytest.Config
     """
     for suite in SUITES:
-        config.addinivalue_line(
-            "markers", f"{suite}: mark test as {suite}"
-        )
+        config.addinivalue_line("markers", f"{suite}: mark test as {suite}")
 
-def pytest_collection_modifyitems(config, items):
-    """
-    Skip tests that are not explicitly under the given suite.
+
+def pytest_collection_modifyitems(config: pytest.Config, items: List[pytest.Item]):
+    """Skip tests that are not explicitly under the given suite.
+
+    Parameters
+    ----------
+    config : pytest.Config
+    items : list of pytest.Item
     """
     skips = {suite: pytest.mark.skip(reason=f"--{suite} not set") for suite in SUITES}
     run = set(name for name in SUITES if config.getoption(f"--{name}"))
