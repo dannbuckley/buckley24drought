6d94992cdb57bfa2a4d8fe70b63d13596b3c9cec
Daniel Buckley <buckley.daniel.n@gmail.com>
2024-01-23 20:44:00 -0700

    Combine SPI, SGI, and correlation plots into mosaic

--- figures/generate_corr_figures.py
+++ figures/generate_corr_figures.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from os.path import exists
 
+from matplotlib.colors import to_rgba
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -152,15 +153,31 @@ def generate_crosscorr_plots():
         plt.close(fig)
 
     def _generate_series_plots(corr_df: pd.DataFrame, meta_df: pd.DataFrame):
-        """Generate individual cross-correlation plots for each SGI record."""
+        """Generate individual cross-correlation plots for each SGI record.
+
+        Parameters
+        ----------
+        corr_df : pandas.DataFrame
+        meta_df : pandas.DataFrame
+        """
         xticks = np.array([1, 3, 6, 12, 24, 48])
         for gwicid in meta_df["gwicid"]:
             corr_val = corr_df.loc[f"SGI_{gwicid}"]
 
             fig, ax = plt.subplots()
-            ax.plot(np.arange(1, 49), corr_val, color="tab:gray")
+            ax.plot(np.arange(1, 49), corr_val, color=to_rgba("tab:gray", 0.6))
             # plot maximum cross-correlation as point
-            ax.plot([np.argmax(corr_val) + 1], [corr_val.max()], "ko")
+            ax.plot(
+                [np.argmax(corr_val) + 1], [corr_val.max()], "ko", label="Max. Corr."
+            )
+            ax.legend()
+            corr_ymin, corr_ymax = ax.get_ylim()
+            ax.axvline(
+                x=np.argmax(corr_val) + 1,
+                ymax=(corr_val.max() - corr_ymin) / (corr_ymax - corr_ymin),
+                color="black",
+                ls="--",
+            )
             ax.set_xlim(0, 49)
             ax.set_xticks(xticks, xticks.astype(str))
             ax.set_xlabel("SPI Accumulation Period")
@@ -177,6 +194,148 @@ def generate_crosscorr_plots():
             )
             plt.close(fig)
 
+    def _generate_spi_sgi_mosaic_plots(
+        area: str, corr_df: pd.DataFrame, meta_df: pd.DataFrame
+    ):
+        """Combine SPI series, SGI series, and cross-correlation series into one figure.
+
+        Parameters
+        ----------
+        area : str
+            Area name as used in the SPI dataframe
+        corr_df : pandas.DataFrame
+        meta_df : pandas.DataFrame
+        """
+        corr_xticks = np.array([1, 3, 6, 12, 24, 48])
+        for gwicid in meta_df["gwicid"]:
+            corr_val = corr_df.loc[f"SGI_{gwicid}"]
+
+            spi_max_per = np.argmax(corr_val) + 1
+            spi_sgi_df = pd.merge(
+                left=pd.read_csv(
+                    f"../index_data/spi/spi_{spi_max_per}.csv",
+                    usecols=["area", "end_date", "SPI"],
+                    parse_dates=["end_date"],
+                )
+                .query(f"area == '{area}'")
+                .drop(columns="area")
+                .sort_values(by="end_date", ascending=True),
+                right=pd.read_csv(
+                    f"../index_data/sgi/sgi_{gwicid}.csv",
+                    usecols=["date", "SGI"],
+                    parse_dates=["date"],
+                ).rename(
+                    columns={
+                        "date": "end_date",
+                    }
+                ),
+                how="left",
+                left_on=["end_date"],
+                right_on=["end_date"],
+            )
+
+            fig = plt.figure(figsize=(10, 4), layout="constrained")
+            ax_dict = fig.subplot_mosaic(
+                mosaic=[["spi", "corr"], ["sgi", "corr"]], width_ratios=[1.5, 1]
+            )
+
+            for _, ax in ax_dict.items():
+                sns.despine(ax=ax)
+
+            # === SPI PLOT ===
+            ax_dict["spi"].axhline(y=0, color="black")
+            ax_dict["spi"].fill_between(
+                x=spi_sgi_df["end_date"],
+                y1=spi_sgi_df["SPI"],
+                y2=0,
+                where=spi_sgi_df["SPI"] > 0,
+                step="mid",
+                edgecolor=to_rgba("tab:blue", 0.8),
+                facecolor=to_rgba("tab:cyan", 0.6),
+                lw=1.5,
+            )
+            ax_dict["spi"].fill_between(
+                x=spi_sgi_df["end_date"],
+                y1=spi_sgi_df["SPI"],
+                y2=0,
+                where=spi_sgi_df["SPI"] <= 0,
+                step="mid",
+                edgecolor=to_rgba("firebrick", 0.8),
+                facecolor=to_rgba("tab:red", 0.6),
+                lw=1.5,
+            )
+            ax_dict["spi"].set_xticks(
+                ax_dict["spi"].get_xticks(), ["" for _ in ax_dict["spi"].get_xticks()]
+            )
+            ax_dict["spi"].set_xlim(
+                spi_sgi_df["end_date"].iloc[0], spi_sgi_df["end_date"].iloc[-1]
+            )
+            ax_dict["spi"].set_ylim(
+                np.min([np.min(spi_sgi_df["SPI"]), -3]),
+                np.max([np.max(spi_sgi_df["SPI"]), 3]),
+            )
+            ax_dict["spi"].set_ylabel(f"SPI-{spi_max_per}")
+            ax_dict["spi"].set_title("(a)", loc="left", fontweight="bold")
+
+            # === SGI PLOT ===
+            ax_dict["sgi"].axhline(y=0, color="black")
+            sns.lineplot(
+                data=spi_sgi_df,
+                x="end_date",
+                y="SGI",
+                color=to_rgba("tab:gray", 0.6),
+                ax=ax_dict["sgi"],
+            )
+            sns.scatterplot(
+                data=spi_sgi_df,
+                x="end_date",
+                y="SGI",
+                color="black",
+                ax=ax_dict["sgi"],
+                zorder=10,
+                s=15,
+            )
+            ax_dict["sgi"].set_xlim(
+                spi_sgi_df["end_date"].iloc[0], spi_sgi_df["end_date"].iloc[-1]
+            )
+            ax_dict["sgi"].set_ylim(
+                np.min([np.min(spi_sgi_df["SGI"]), -3]),
+                np.max([np.max(spi_sgi_df["SGI"]), 3]),
+            )
+            ax_dict["sgi"].set_xlabel("Date")
+            ax_dict["sgi"].set_ylabel(f"SGI (GWIC {gwicid})")
+            ax_dict["sgi"].set_title("(b)", loc="left", fontweight="bold")
+
+            # === CROSS-CORRELATION PLOT ===
+            ax_dict["corr"].plot(
+                np.arange(1, 49), corr_val, color=to_rgba("tab:gray", 0.6)
+            )
+            ax_dict["corr"].plot(
+                [spi_max_per], [corr_val.max()], "ko", label="Max. Corr."
+            )
+            ax_dict["corr"].legend()
+            corr_ymin, corr_ymax = ax_dict["corr"].get_ylim()
+            ax_dict["corr"].axvline(
+                x=spi_max_per,
+                ymax=(corr_val.max() - corr_ymin) / (corr_ymax - corr_ymin),
+                color="black",
+                ls="--",
+            )
+            ax_dict["corr"].set_xlim(0, 49)
+            ax_dict["corr"].set_xticks(corr_xticks, corr_xticks.astype(str))
+            ax_dict["corr"].set_xlabel("SPI Accumulation Period")
+            ax_dict["corr"].set_title("(c)", loc="left", fontweight="bold")
+
+            fig.savefig(
+                fname=f"corr/cross/mosaic/maxspi_sgi_{gwicid}.svg",
+                dpi=300,
+                format="svg",
+            )
+            print(
+                f"Mosaic SPI-SGI figure generated at corr/cross/mosaic/maxspi_sgi_{gwicid}.svg"
+            )
+            plt.close(fig)
+
     bv_corr = _get_cross_corr_df("Bitterroot", bv_meta, spi_df)
     gv_corr = _get_cross_corr_df("Gallatin", gv_meta, spi_df)
     pv_corr = _get_cross_corr_df("Paradise Valley", pv_meta, spi_df)
@@ -185,6 +344,9 @@ def generate_crosscorr_plots():
     _generate_series_plots(bv_corr, bv_meta)
     _generate_series_plots(gv_corr, gv_meta)
     _generate_series_plots(pv_corr, pv_meta)
+    _generate_spi_sgi_mosaic_plots("Bitterroot", bv_corr, bv_meta)
+    _generate_spi_sgi_mosaic_plots("Gallatin", gv_corr, gv_meta)
+    _generate_spi_sgi_mosaic_plots("Paradise Valley", pv_corr, pv_meta)
 
 
 parser = ArgumentParser()
